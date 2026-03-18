7da76d9a1339c9d0bbe155a4ef80dd9a7abc160f
Yousaf Nabi <yousaf@pactflow.io>
2026-03-18 18:22:47 +0000

    chore(deps): update pact-broker-client to 0.6.3

--- pact-broker-client.rb
+++ pact-broker-client.rb
@@ -16,25 +16,25 @@ class PactBrokerClient < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.5.0/pact-broker-cli-x86_64-macos"
-      sha256 "3e4ef399c2b2fd615c8a3d84af7d894b50540ce40a7f882c279f05d7aaa2332c"
+      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.6.3/pact-broker-cli-x86_64-macos"
+      sha256 "3a7497e1956a60c2b898794dcd03981462fd1909ba7b4983e57d303eae7d9a93"
     end
 
     on_arm do
-      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.5.0/pact-broker-cli-aarch64-macos"
-      sha256 "447fe8990ec6716bd3688e570c8a080bb685ae1d2427aa223b41766786b26f79"
+      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.6.3/pact-broker-cli-aarch64-macos"
+      sha256 "3b1f0e834f604a39caaacfc9ff05ec272812f1c924580c5fe66551317579f6fa"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.5.0/pact-broker-cli-x86_64-linux-musl"
-      sha256 "f6ef13685b78b9eb92285bdfd64f45317a3e71433e9e873d2835f13c17bf832e"
+      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.6.3/pact-broker-cli-x86_64-linux-musl"
+      sha256 "e42ee13d4cb628ba1e0ca09c40ffe528f4866dcfbf0d11808c73818c97f2038d"
     end
 
     on_arm do
-      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.5.0/pact-broker-cli-aarch64-linux-musl"
-      sha256 "f3f3f10afc907e3db66e66f37379d8cda09e958a411b582ab46d85ba1bb160e2"
+      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.6.3/pact-broker-cli-aarch64-linux-musl"
+      sha256 "23b6628b80f6c7a8a88e8bcfdca472e932e45cb156fe49b20dd7e6a8347ad0cc"
     end
   end
 
