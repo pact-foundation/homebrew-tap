ebb48fcb6d8e7f33fa664116fc0cf71cfed4326b
Yousaf Nabi <yousaf@pactflow.io>
2026-06-29 14:02:46 +0100

    chore(deps): update pact-standalone to v2.6.4

--- pact-legacy.rb
+++ pact-legacy.rb
@@ -1,28 +1,28 @@
 class PactLegacy < Formula
   desc "Standalone pact CLI executable using the Ruby Pact impl and Traveling Ruby"
   homepage "https://github.com/pact-foundation/pact-standalone"
-  version "2.6.3"
+  version "2.6.4"
   license "MIT"
   deprecate! date: "2027-01-01", because: :reason, replacement_formula: "pact-foundation/tap/pact"
 
   on_macos do
     on_intel do
-      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.6.3/pact-2.6.3-macos-x86_64.tar.gz"
-      sha256 "505a98e5a48ea3f273bcc1b0e8b48acbe16e3740fa8f4a57915ce53b4dd603d1"
+      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.6.4/pact-2.6.4-macos-x86_64.tar.gz"
+      sha256 "fe23549d3457037dace47727b4c6c4504dc726d342c8440571a24b0291949d42"
     end
     on_arm do
-      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.6.3/pact-2.6.3-macos-arm64.tar.gz"
-      sha256 "3051d29607b9ddeceb816d848c25d4eb210e7e681d2a2951ce2e3df72d13deae"
+      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.6.4/pact-2.6.4-macos-arm64.tar.gz"
+      sha256 "2d85da2bdfd3cca99fb98184e14b10fda8cf27bc12d124648d6502444e2fbc61"
     end
   end
   on_linux do
     on_intel do
-      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.6.3/pact-2.6.3-linux-x86_64.tar.gz"
-      sha256 "02e4489bc946a80ebe53633f692f6d75b14b58d9e9030b0008697c91b0ce0154"
+      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.6.4/pact-2.6.4-linux-x86_64.tar.gz"
+      sha256 "c0d77f4ad390ed8f56a1b46e9f56d4d656b0128202e95ea067a6dacc00b7cbbf"
     end
     on_arm do
-      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.6.3/pact-2.6.3-linux-arm64.tar.gz"
-      sha256 "d16a623406204476f36ca210a9e90f8f688ace173ce22c8da82f88a08c5f943f"
+      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.6.4/pact-2.6.4-linux-arm64.tar.gz"
+      sha256 "8b7541ef6a5d1742436144b945cf6f26ad5a6f82d9ab5a6ec5dfac548187b33d"
     end
   end
 
