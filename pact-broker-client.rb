c60f90b751286186645216f58a5c05dad3ae161a
YOU54F <YOU54F@users.noreply.github.com>
2026-09-16 16:05:06 +0000

    chore(release): Update pact-broker-cli version to v0.8.8

--- pact-broker-client.rb
+++ pact-broker-client.rb
@@ -16,25 +16,25 @@ class PactBrokerClient < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.8.6/pact-broker-cli-x86_64-apple-darwin.tar.xz"
-      sha256 "02c3888f4ae5683f7d2bf99fc4838bc349216fe5fe925e03bd8fd7896ff6cbb9"
+      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.8.8/pact-broker-cli-x86_64-apple-darwin.tar.xz"
+      sha256 "01f3a908e0b5c5fd1dda5a062fdc023810f7a982f2101ef31d316f266b1d986e"
     end
 
     on_arm do
-      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.8.6/pact-broker-cli-aarch64-apple-darwin.tar.xz"
-      sha256 "f399dd9346ac693d987b9509b495e90365bdbdf7aacbc4ff09ebc34b894fbd2a"
+      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.8.8/pact-broker-cli-aarch64-apple-darwin.tar.xz"
+      sha256 "a571c9264e57262163b767d3f05f977d0457bc8fdeb99f07aec4b83c9f10aaf0"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.8.6/pact-broker-cli-x86_64-unknown-linux-musl.tar.xz"
-      sha256 "ffaa0cf297e36fbafa4bacc5c15e3ae65bab33c46b7af42f03d9466cbf6e165c"
+      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.8.8/pact-broker-cli-x86_64-unknown-linux-musl.tar.xz"
+      sha256 "c12745595ab437ebd80bfa43d66da656536162e64a6409277a84d40ecd034e2c"
     end
 
     on_arm do
-      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.8.6/pact-broker-cli-aarch64-unknown-linux-musl.tar.xz"
-      sha256 "78a09ad68fa767b73a78e1a5986ce6f50c48f1698c5b988e2e8660e01b23f1d1"
+      url "https://github.com/pact-foundation/pact-broker-cli/releases/download/v0.8.8/pact-broker-cli-aarch64-unknown-linux-musl.tar.xz"
+      sha256 "d1da079f6c4583cc4af84dbea6406e5d0a3f0e3ae603b850c90592090e55d315"
     end
   end
 
